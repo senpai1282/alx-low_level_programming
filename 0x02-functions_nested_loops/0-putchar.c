89caf4b6f1eb4029bc0032f605517f5e181f23c3
senpai1282 <farhi.ayoub.hamza@gmail.com>
2023-09-12 12:23:26 -0700

    this is my task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,19 @@
-#include <unistd.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Return:0
+ */
 
 int main(void)
 {
-    char str[] = "_putchar\n";
-    write(1, str, sizeof(str) - 1);
-    return (0);
+char c[9] = "_putchar";
+int i = 0;
+while (i < 8)
+{
+_putchar(c[i]);
+i++;
+}
+_putchar('\n');
+return (0);
 }
