d19bff07bb7354cbeb5ab110a55c60a7aa6b2d4c
senpai1282 <farhi.ayoub.hamza@gmail.com>
2023-11-15 11:37:36 -0800

    hi

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,38 +1,36 @@
 #include "main.h"
 
 /**
- * check_square - Checks for the natural square root of n.
- * @n: Number to find the square root of.
- * @i: Iterator to check for square root.
+ * helperFunction - Finds the square root recursively using binary search.
+ * @num: Number to find the square root of.
+ * @pSqrt: Potential square root to be checked.
  *
- * Return: Square root of n if found, otherwise -1.
+ * Return: The square root of num if found, otherwise -1.
  */
 int helperFunction(int num, int pSqrt)
 {
-	if ((pSqrt * pSqrt) == num)
-	{
-		return (pSqrt);
-	}
-	else
-	{
-		if ((pSqrt * pSqrt) > num)
-			return (-1);
-		else
-			return (helperFunction(num, pSqrt + 1));
+    if (pSqrt * pSqrt == num) {
+        return pSqrt;
+    }
 
-	}
+    if (pSqrt * pSqrt > num) {
+        return -1;
+    }
+
+    return helperFunction(num, pSqrt + 1);
 }
 
 /**
  * _sqrt_recursion - Returns the natural square root of a number.
  * @n: The number to find the square root of.
  *
- * Return: Square root of n if found, otherwise -1.
+ * Return: The square root of n if found, otherwise -1.
  */
 int _sqrt_recursion(int n)
 {
-	if (n < 0)
-		return (-1);
-	else
-		return (helperFunction(n, 0));
+    if (n < 0) {
+        return -1;
+    }
+
+    return helperFunction(n, 0);
 }
