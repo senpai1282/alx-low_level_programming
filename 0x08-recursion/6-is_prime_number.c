ab1c162f2ea3fab66d5434322a250e8c038c3351
senpai1282 <farhi.ayoub.hamza@gmail.com>
2023-11-15 11:54:40 -0800

    hi

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,16 +9,16 @@
  */
 int check_prime(int n, int i)
 {
-	if (n <= 2)
-		return (n == 2);
+    if (n <= 2)
+        return (n == 2);
 
-	if (n % i == 0)
-		return (0);
+    if (n % i == 0)
+        return (0);
 
-	if (i * i > n)
-		return (1);
+    if (i * i > n)
+        return (1);
 
-	return check_prime(n, i + 1);
+    return check_prime(n, i + 1);
 }
 
 /**
@@ -29,8 +29,8 @@ int check_prime(int n, int i)
  */
 int is_prime_number(int n)
 {
-	if (n <= 1)
-		return (0);
+    if (n <= 1)
+        return (0);
 
-	return (check_prime(n, 2));
+    return (check_prime(n, 2));
 }
