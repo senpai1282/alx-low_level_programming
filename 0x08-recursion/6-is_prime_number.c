cfdb9fc3e2da54335a786162fc69cf3d980ecf1f
senpai1282 <farhi.ayoub.hamza@gmail.com>
2023-11-15 11:48:04 -0800

    hi

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,44 +1,34 @@
 #include "main.h"
-
 /**
- * check_prime - Checks if a number is prime.
- * @n: Number to check for being prime.
- * @i: Iterator to check divisibility.
+ * check_prime - Helper function to check if a number is prime.
+ * @n: Number to be checked.
+ * @i: Iterator variable for checking divisibility.
  *
- * Return: 1 if prime, 0 if not.
+ * Return: 1 if n is prime, otherwise 0.
  */
-int helperFunction(int num, int i)
+int check_prime(int n, int i)
 {
-	if (i < num)
-	{
-		if (num % i == 0)
-		{
-			return (0);
-		}
-		else
-		{
-			return (helperFunction(num, i + 1));
-		}
-	}
-	else
-	{
-		return (1);
-	}
+    if (n <= 2) {
+        return (n == 2);
+    }
+    if (n % i == 0) {
+        return 0;
+    }
+    if (i * i > n) {
+        return 1;
+    }
+    return check_prime(n, i + 1);
 }
 /**
- * is_prime_number - Checks if a number is prime.
- * @n: The number to check.
+ * is_prime_number - Checks if an input integer is a prime number.
+ * @n: Number to be checked.
  *
- * Return: 1 if prime, 0 if not.
+ * Return: 1 if n is prime, otherwise 0.
  */
 int is_prime_number(int n)
 {
-	if (n <= 1)
-	{
-		return (0);
-	}
-	else
-	{
-		return (helperFunction(n, 2));
-	}
+    if (n <= 1) {
+        return 0;
+    }
+    return check_prime(n, 2);
 }
