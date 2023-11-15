893caf2b8c2ccab2b57a3a0e3c9aeb19198e454a
senpai1282 <farhi.ayoub.hamza@gmail.com>
2023-11-15 10:19:27 -0800

    hi

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,13 +4,13 @@
  * @s: The string to be printed.
  */
 
-	void _puts_recursion(char *s)
-	{	
-	    	if (*s == '\0')
-		{
-			_putchar('\n'); 
-			return;
-		}
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n'); 
+		return;
+	}
 	_putchar(*s);
 	_puts_recursion(s + 1);
-	}
+}
