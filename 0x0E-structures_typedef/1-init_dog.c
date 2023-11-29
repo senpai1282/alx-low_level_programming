baa132fbc0f076e168885ef1951c008ad139f5ae
senpai1282 <farhi.ayoub.hamza@gmail.com>
2023-11-28 18:30:39 -0800

    hi

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,12 @@
+#include <stddef.h>
+#include "dog.h"
+
+void init_dog(struct dog *d, char *name, float age, char *owner) {
+    if (d != NULL) {
+        d->name = name;
+        d->age = age;
+        d->owner = owner;
+    }
+}
+
+
---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "dog.h"
+
+void print_dog(struct dog *d) {
+    if (d != NULL) {
+        printf("Name: %s\n", (d->name != NULL) ? d->name : "(nil)");
+        printf("Age: %.6f\n", d->age);
+        printf("Owner: %s\n", (d->owner != NULL) ? d->owner : "(nil)");
+    }
+}
+
