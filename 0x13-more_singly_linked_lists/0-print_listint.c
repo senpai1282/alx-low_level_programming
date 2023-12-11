b8bfcdf80dff09f21b83741e8a9b1239ec60a022
senpai1282 <farhi.ayoub.hamza@gmail.com>
2023-12-11 07:39:12 -0800

    task_0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,16 +10,15 @@
  * Return: nbr of nodes
  */
 size_t print_listint(const listint_t *h)
-{
-    size_t count = 0;
+	{
+		size_t count = 0;
 
-    while (h != NULL)
-    {
-        printf("%d\n", h->n);
-        h = h->next;
-        count++;
-    }
-
-    return count;
-}
+		while (h != NULL)
+			{
+				printf("%d\n", h->n);
+				h = h->next;
+				count++;
+			}
+		return (count);
+	}
 
