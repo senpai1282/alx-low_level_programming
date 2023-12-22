fb1709dbcb3e470d526e768a16ccb6cd5cc692d2
senpai1282 <farhi.ayoub.hamza@gmail.com>
2023-12-22 06:42:23 -0800

    hi

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,32 +1,33 @@
 #include "main.h"
 
 /**
- * create_file - Creates a file and writes a text to it
- * @filename: Name of the file to create
- * @text_content: NULL terminated string to write to the file
+ * create_file - Creates file.
+ * @filename: A pointer to the name of file created
+ * @text_content: A pointer to a string to write to file
  *
- * Return: 1 on success, -1 on failure
+ * Return: If the function fails -1
+ *         Otherwise - 1
  */
-int create_file(const char *filename, char *text_content) {
-    int file_descriptor, bytes_written;
-    mode_t mode = S_IRUSR | S_IWUSR;
+int create_file(const char *filename, char *text_content)
+{
+	int o, w, len = 0;
 
-    if (filename == NULL)
-        return -1;
+	if (filename == NULL)
+		return (-1);
 
-    file_descriptor = open(filename, O_WRONLY | O_CREAT | O_TRUNC, mode);
-    if (file_descriptor == -1)
-        return -1;
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
 
-    if (text_content != NULL) {
-        bytes_written = write(file_descriptor, text_content, strlen(text_content));
-        if (bytes_written == -1) {
-            close(file_descriptor);
-            return -1;
-        }
-    }
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content, len);
 
-    close(file_descriptor);
-    return 1;
+	if (o == -1 || w == -1)
+		return (-1);
+
+	close(o);
+
+	return (1);
 }
-
