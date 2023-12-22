20fc93d02a2e90ce9f74590b8819e5e7d15bc751
senpai1282 <farhi.ayoub.hamza@gmail.com>
2023-12-22 06:25:31 -0800

    hi

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,24 +1,53 @@
 #include "main.h"
+
 /**
- * read_textfile - read txt from a file and print it
- * @filename : name of the file to read
- * @letters : nbr of bytes readed/printed
+ * read_textfile - Reads a text file and prints it to the POSIX standard output
+ * @filename: Name of the file to read
+ * @letters: Number of letters to read and print
+ *
+ * Return: The actual number of letters it could read and print, or 0 on failure
  */
-ssize_t read_textfile(const char *filename, size_t letters);
-{
-	int fd;
-	ssize_t bytes;
-	char buf[READ_BUF_SIZE * 8 ];
+ssize_t read_textfile(const char *filename, size_t letters) {
+    int file_descriptor, bytes_read, bytes_written;
+    char *buffer;
 
-		if (!filename || !letters)
-			return (0);
-		fd = open(filename, O_RDONLY);
-		if (fd == -1)
-			return (0);
-	bytes = read(fd, &buf[0], letters);
-	bytes = write(STDOUT_FILENO, &buf[0], bytes);
-	close(fd);
-	return (bytes);
+    if (filename == NULL)
+        return 0;
+
+    /* Allocate memory for the buffer */
+    buffer = malloc(sizeof(char) * (letters + 1));
+    if (buffer == NULL)
+        return 0;
+
+    /* Open the file */
+    file_descriptor = open(filename, O_RDONLY);
+    if (file_descriptor == -1) {
+        free(buffer);
+        return 0;
+    }
+
+    /* Read from the file */
+    bytes_read = read(file_descriptor, buffer, letters);
+    if (bytes_read == -1) {
+        free(buffer);
+        close(file_descriptor);
+        return 0;
+    }
+
+    /* Null-terminate the buffer */
+    buffer[bytes_read] = '\0';
+
+    /* Write to the standard output */
+    bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+    if (bytes_written == -1 || bytes_written != bytes_read) {
+        free(buffer);
+        close(file_descriptor);
+        return 0;
+    }
+
+    /* Cleanup and return the number of bytes read */
+    free(buffer);
+    close(file_descriptor);
+    return bytes_read;
 }
-
 
